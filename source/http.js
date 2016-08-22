9e09e9c346736c4360a0b5602a81a52e2e4a0b05
nikikilbertus <kilbertus.niki@gmail.com>
2016-08-22 14:27:07 +0200

    fixed typo

--- source/http.js
+++ source/http.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
-http.get('http://httpbin.org/bytes/4', (res) => {
+http.get('http://httpbin.org/bytes/4', (req) => {
     var str = '';
-    res.on('data', (chunk) => {str += chunk;});
-    res.on('end', () => {console.log(str);});
+    req.on('data', (chunk) => {str += chunk;});
+    req.on('end', () => {console.log(str);});
 }).on('error', (e) => {console.log(`error: ${e.message}`);});
--- source/https.js
+++ source/https.js
@@ -1,9 +1,9 @@
 var https = require('https');
 
 https.get('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new',
-(res) => {
-    console.log('headers: ', res.headers);
+(req) => {
+    console.log('headers: ', req.headers);
     var str = '';
-    res.on('data', (chunk) => {str += chunk;});
-    res.on('end', () => {console.log(str);});
+    req.on('data', (chunk) => {str += chunk;});
+    req.on('end', () => {console.log(str);});
 }).on('error', (e) => {console.error(`error: ${e.message}`);});
